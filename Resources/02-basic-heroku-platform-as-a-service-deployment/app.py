b0857f9158560196da25abe0bd0996005677ac81
asapanna rakesh <rakeshark22@gmail.com>
2019-09-28 22:26:46 +0530

    heroku added

--- Resources/02-basic-heroku-platform-as-a-service-deployment/app.py
+++ Resources/02-basic-heroku-platform-as-a-service-deployment/app.py
@@ -2,10 +2,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-# Initialize a flask app [Important!]
 app = Flask(__name__)
-
-# Read generated model from model.py
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
@@ -17,9 +14,7 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    # request.form.values() returns all values in a form
     int_features = [int(x) for x in request.form.values()]
-    # convert to np array
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
@@ -27,16 +22,6 @@ def predict():
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
